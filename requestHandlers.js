45fc17086c55bfe51de4cbf776b70348a3eb0880
Daniel Ossorio <daniel@ossorio.me>
2014-04-25 14:56:20 +0100

    being able to upload pictures and show them

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,7 +1,8 @@
 var querystring = require("querystring"),
-    fs = require('fs');
+    fs = require('fs'),
+    formidable = require('formidable');
 
-function start(response, postData) {
+function start(response, request) {
     console.log("Request handler 'start' was called");
     var content = "empty";
 
@@ -22,22 +23,36 @@ function start(response, postData) {
     response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
     console.log("Request handler 'upload' was called");
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("You have sent: " + querystring.parse(postData).text);
+    var form = new formidable.IncomingForm();
+    console.log("About to parse");
+
+    form.parse(request, function (error, fields, files) {
+        console.log("parsing done");
+        fs.rename(files.upload.path, "/tmp/test.png", function (error) {
+            if (error) {
+                fs.unlink("/tmp/test.png");
+                fs.rename(files.upload.path, "/tmp/test.png");
+            }
+        });
+    });
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("received image: <br/>");
+    response.write("<img src='/show'/>");
     response.end();
 }
 
 function show(response, postData) {
     console.log("Request handler 'show' was called");
-    fs.readFile("/tmp/test.png", "binary", function(error, file){
-        if(error){
+    fs.readFile("/tmp/test.png", "binary", function (error, file) {
+        if (error) {
             response.writeHead(500, {"Content-Type": "text/plain"});
             response.write(error + "\n");
             response.end();
-        }else{
+        } else {
             response.writeHead(200, {"Content-Type": "image/png"});
             response.write(file, "binary");
             response.end();
