2ae9a172c094ba21513029f85200739d9678cdd8
Daniel Ossorio <daniel@ossorio.me>
2014-04-25 16:41:32 +0100

    functions test

---
+++ tests/function-tests.js
@@ -0,0 +1,17 @@
+
+var schedule = function(timeout, callbackfunction){
+  return {
+      start: function(){
+          setTimeout(callbackfunction, timeout)
+      }
+  };
+};
+
+(function(){
+    var timeout = 1000;
+    var count = 0;
+    schedule(timeout, function doStuff(){
+        console.log(++ count);
+        schedule(timeout, doStuff);
+    }).start();
+})();
