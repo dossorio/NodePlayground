6c47bdece49f01c3301d8e99af0809f757f7200e
Daniel Ossorio <daniel@ossorio.me>
2014-04-25 10:22:56 +0100

    bug in a route

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,10 +1,16 @@
-
-function start(){
+function start() {
     console.log("Request handler 'start' was called");
+
+    function sleep(milliSeconds) {
+        var startTime = new Date().getTime();
+        while (new Date().getTime() < startTime + milliSeconds);
+    }
+
+    sleep(10000);
     return "Hello Start";
 }
 
-function upload(){
+function upload() {
     console.log("Request handler 'upload' was called");
     return "Hello Upload";
 }
