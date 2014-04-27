a5b1f9e7a1a86276d0029194eca61523adce4a71
Daniel Ossorio <daniel@ossorio.me>
2014-04-27 18:08:12 +0100

    testing with connect module

---
+++ tests/connect-test.js
@@ -0,0 +1,21 @@
+/**
+ * Created by dossorio on 27/04/2014.
+ */
+
+function logger(req, res, next) {
+    console.log('Request received for: ' + req.url);
+    next();
+}
+
+function hello(req, res, next) {
+    res.setHeader("Content-Type", "text/plain");
+    res. end("Hello my friend");
+}
+
+var connect = require('connect');
+var app = connect();
+
+app
+    .use(logger)
+    .use(hello)
+    .listen(8888);
