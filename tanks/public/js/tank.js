a116af615f6ec72c728338b3672ad014132ab22f
Daniel Ossorio <daniel@ossorio.me>
2014-04-29 11:37:28 +0100

    preparing tank

---
+++ tanks/public/js/tank.js
@@ -0,0 +1,19 @@
+/**
+ * Created by dossorio on 29/04/2014.
+ */
+
+var tank = {
+    settings: {
+        player: null,
+        color: null,
+        height: 25,
+        width: 25,
+        x: null,
+        y: null
+    },
+    init: function(){
+        s = this.settings;
+    },
+    move: function(){},
+    shoot: function(){}
+};
---
+++ tanks/public/js/tanks.js
@@ -0,0 +1,13 @@
+/**
+ * Created by dossorio on 29/04/2014.
+ */
+
+$.fn.tanks = function () {
+
+    /* pg = Canvas Playground */
+    var pg = this;
+};
+
+$(function () {
+    $('.tanks').tanks();
+});
