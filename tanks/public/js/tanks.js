941fc16ed4bec066ec8d62e94259df91220fb3f2
Daniel Ossorio <daniel@ossorio.me>
2014-04-29 16:19:22 +0100

    filling the canvas

--- tanks/public/js/tanks.js
+++ tanks/public/js/tanks.js
@@ -5,7 +5,33 @@
 $.fn.tanks = function () {
 
     /* pg = Canvas Playground */
-    var pg = this;
+    var pg = document.createElement('canvas'),
+        pgContext = pg.getContext('2d');
+
+    $(pg)
+        .height('100%')
+        .width('100%')
+        .appendTo(this);
+
+    pgContext.fillRect(50, 25, 150, 100);
+
+    /* keys 37, 38, 39 and 40 */
+    $(window).keypress(function(e){
+        switch(e.keyCode){
+            case 37:
+                console.log('left');
+                break;
+            case 38:
+                console.log('up');
+                break;
+            case 39:
+                console.log('right');
+                break;
+            case 40:
+                console.log('down');
+                break;
+        }
+    });
 };
 
 $(function () {
